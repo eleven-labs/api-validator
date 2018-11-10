53386100e6f607d42dabc5d90880b975d3c9e074
Guillem CANAL <gcanal@eleven-labs.com>
2018-11-10 10:23:18 +0100

    feat: better travis test matrix which reflect all supported version of PHP and dependencies

--- .travis.coverage.sh
+++ .travis.coverage.sh
@@ -3,10 +3,8 @@
 set -e
 
 main() {
-    if [ "$TRAVIS_PHP_VERSION" = '7.2' ]; then
-        wget -q https://scrutinizer-ci.com/ocular.phar
-        php ocular.phar code-coverage:upload --format=php-clover ./build/phpunit.coverage.xml
-    fi
+    wget -q https://scrutinizer-ci.com/ocular.phar
+    php ocular.phar code-coverage:upload --format=php-clover ./build/phpunit.coverage.xml
 }
 
 main
